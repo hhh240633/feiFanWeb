7de4935ffdba3be10e8267201597f0f19fd11588
hhh240633 <63026236+hhh240633@users.noreply.github.com>
2022-08-02 23:11:29 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,6 +13,11 @@ jQuery(document).ready(function( $ ) {
     return false;
   });
 
+  $('.logo').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
   // Initiate the wowjs animation library
   new WOW().init();
 
@@ -98,6 +103,40 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
+//所有链接滑动
+  $('.nav-menu2 a').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if ($('#header').length) {
+          top_space = $('#header').outerHeight();
+
+          if( ! $('#header').hasClass('header-fixed') ) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
   // Header scroll class
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
@@ -121,7 +160,7 @@ jQuery(document).ready(function( $ ) {
       if (direction == 'left') $(this).carousel('next');
       if (direction == 'right') $(this).carousel('prev');
     },
-    allowPageScroll:"vertical"
+    allowPageScroll:"vertical",
   });
 
   // Skills section
